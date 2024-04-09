fe48bf347a2a947ca9183fc5e1a70a4841541887
Pablo Muñoz <pablomunoz@besoccer.com>
2024-04-09 17:59:46 +0200

    feat panel switch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use BezhanSalleh\PanelSwitch\PanelSwitch;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,9 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Gate::define('viewPulse', function (User $user) {
-        //     return $user->isAdmin();
-        // });
+        PanelSwitch::configureUsing(function (PanelSwitch $panelSwitch) {
+            $panelSwitch->simple()
+            ->labels([
+                'admin' => 'Custom Admin Label',
+                'general_manager' => __('General Manager')
+            ])
+            ->icons([
+                'validPanelId1' => 'heroicon-o-square-2-stack',
+                'validPanelId2' => 'heroicon-o-star',
+            ], $asImage = false);
+        });
 
         //
     }
